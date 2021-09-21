50af81ccca1969943068dded4af9a8fa5aa32f82
Jenny Zonka <jennycade@gmail.com>
2021-09-21 15:05:43 -0700

    Removed dependency on old fakedb

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import dataHandler, { DbContext } from './fakedb';
-
 ReactDOM.render(
-  <DbContext.Provider value={ dataHandler() }>
+  <React.StrictMode>
     <App />
-  </DbContext.Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
