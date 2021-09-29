8b6291c62f9a1b2686291fd10b9978e16c3a4783
Jenny Zonka <jennycade@gmail.com>
2021-09-29 14:43:35 -0700

    Implemented plain text editing and loading/saving to database using SlateJS

--- src/LiveBlock.js
+++ src/LiveBlock.js
@@ -1,16 +1,46 @@
-import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import { useState, useMemo } from 'react';
+import { Slate, Editable, withReact } from 'slate-react';
+import { createEditor } from 'slate';
 
 const LiveBlock = (props) => {
   // props
-  
+  const { updateContent } = props;
 
-  const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
+  // state
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  // pull value from props
+  const [value, setValue] = useState(
+    JSON.parse(props.textContent)
+  //   JSON.parse(props.textContent) || [
+  //   {
+  //     type: 'paragraph',
+  //     children: [{ text: 'A line of text in a paragraph.' }],
+  //   },
+  // ]
   );
 
-  return <Editor editorState={ editorState } onChange={ setEditorState } />;
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={value => {
+        setValue(value);
+
+        const isAstChange = editor.operations.some(
+          op => 'set_selection' !== op.type
+        );
+        if (isAstChange) {
+          // save the value to the database
+          const content = JSON.stringify(value);
+          updateContent(content);
+        }
+      }
+      }
+    >
+      <Editable />
+    </Slate>
+  );
 }
 
 export default LiveBlock;
