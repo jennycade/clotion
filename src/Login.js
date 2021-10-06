0099d64de2ee008bcaca4b7f1c3e40fa97a6486a
Jenny Zonka <jennycade@gmail.com>
2021-10-06 09:24:59 -0700

    Made sign in/sign up form behave like Notion's with email, password, and forgot password forms/buttons

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,19 @@
 import './Login.css';
 
 import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 const Login = (props) => {
   // state
   const [submittedEmail, setSubmittedEmail] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [forgotEmail, setForgotPassword] = useState(false);
+
+  // clear out password if submittedEmail changes
+  useEffect(() => {
+    setPassword('')
+  }, [submittedEmail, setPassword]);
 
   // controlled input
   const handleEmailChange = (event) => {
@@ -22,6 +29,13 @@ const Login = (props) => {
     setSubmittedEmail(true);
   }
 
+  const handleForgotPassword = (event) => {
+    setForgotPassword(true);
+  }
+  const handleUnforgotPassword = (event) => {
+    setForgotPassword(false);
+  }
+
 
   return (
     <div className="login">
@@ -30,25 +44,41 @@ const Login = (props) => {
       <button className="googleSignIn">Continue with Google</button>
       <button className="anonSignIn">Continue anonymously</button>
 
-      <label for="emailInput">Email</label>
+      <label htmlFor="emailInput">Email</label>
       <input id="emailInput" type="text" placeholder="Enter your email address…" value={ email } onChange={ handleEmailChange } />
       
-      <button className="emailSignIn">
+      { !submittedEmail && !forgotEmail &&
+      <button className="emailSignIn" onClick={ submitEmail }>
         Continue with email
       </button>
+      }
+
+      {
+        forgotEmail &&
+        <button className="emailSignIn">
+          Send reset link
+        </button>
+      }
 
       { submittedEmail &&
-      <label for="passwordInput">Password</label> }
+      <label htmlFor="passwordInput">Password</label> }
       { submittedEmail && 
       <input id="passwordInput" type="password" value={ password } onChange={ handlePasswordChange } />
       }
       { submittedEmail && 
-      <button className="passwordSignIn">
+      <button className="emailSignIn">
         Continue with email
       </button>
       }
 
-      <button className="nonButton">Forgot password?</button>
+      { !forgotEmail &&
+      <button className="nonButton" onClick={ handleForgotPassword }>Forgot password?</button>
+      }
+
+      { forgotEmail &&
+        <button className="nonButton" onClick={ handleUnforgotPassword }>Continue with email</button>
+      }
+
 
 
     </div>
